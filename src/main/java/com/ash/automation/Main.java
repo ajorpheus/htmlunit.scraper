ca71577549c2c822c9b433517a0611171431e2fe
Ashutosh Jindal <ashutosh.jindal@monitise.com>
2016-05-31 12:02:34 +0100

    Fix verbose logging by excluding other sources for SLF4J bindings
    The log output was verbose enough to affect the performance of the
    scrapers. This was due to multiple SLF4J bindings on the classpath
    resulting in log4j.properties not having an effect. This was fixed by
    excluding other sources of SLF4J bindings to setup the log system
    properly.
    
    Change-Id: I39fac8ee26b146548ef4f8ec4953bd3807cb4ff7

--- src/main/java/com/ash/automation/Main.java
+++ src/main/java/com/ash/automation/Main.java
@@ -1,13 +1,35 @@
 package com.ash.automation;
 
-import org.apache.commons.lang.StringUtils;
+import java.util.logging.Level;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by az on 30/11/2014.
  */
 public class Main {
+
+    public static void turnOffHtmlUnitLogging(){
+        java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+        System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+        System.getProperties().put("org.apache.commons.logging.simplelog.defaultlog", "OFF");
+        LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+        java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(Level.OFF);
+        java.util.logging.Logger.getLogger("org.apache.commons.httpclient").setLevel(Level.OFF);
+        java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit.javascript.StrictErrorReporter").setLevel(Level.OFF);
+        java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit.javascript.host.ActiveXObject").setLevel(Level.OFF);
+        java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit.javascript.host.html.HTMLDocument").setLevel(Level.OFF);
+        java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit.html.HtmlScript").setLevel(Level.OFF);
+        java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit.javascript.host.WindowProxy").setLevel(Level.OFF);
+        java.util.logging.Logger.getLogger("org.apache").setLevel(Level.OFF);
+    }
+
+
     public static void main(String[] args) throws Exception {
+        turnOffHtmlUnitLogging();
+
         String scraperName = System.getProperty("scraper");
         scraperName = StringUtils.defaultIfEmpty(scraperName, "scraperImplBins");
         System.out.println(scraperName);
